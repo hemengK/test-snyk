b7f3ead01b37342f52c6d35691450591457f4160
hemeng <517505440@QQ.COM>
2024-03-05 15:18:58 +0800

    666666

---
+++ src/main/java/com/example/demo/test/SonarTest.java
@@ -0,0 +1,47 @@
+package com.example.demo.test;
+
+import java.util.regex.Pattern;
+
+/**
+ * @Description
+ * @Author admin
+ * @Date 2024-03-04 17:14
+ */
+public class SonarTest {
+
+    private static Pattern IMPORT_PATTERN = Pattern.compile("import(?:\\s+)(?:static\\s+)?(.*)(?:\\s*);");
+
+    public void test(){
+        String k = null;
+        if(k=="code") {
+
+        } else if (k=="name")  {
+
+        } else if (k=="age") {
+
+        } else if (k=="address") {
+
+        } else if (k=="phone") {
+
+        } else if (k=="email") {
+
+        } else if (k=="password") {
+
+        } else if (k=="sex") {
+
+        } else if (k=="birthday") {
+
+        } else if (k=="idCard") {
+
+        } else if (k=="bankCard") {
+
+        } else if (k=="code") {
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
